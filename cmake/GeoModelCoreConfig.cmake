d170cec98c72e0aa37bc1bcc6d7c92a51bcbc102
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2019-04-02 14:25:08 +0200

    removed GeoModelUtilities

--- cmake/GeoModelCoreConfig.cmake
+++ cmake/GeoModelCoreConfig.cmake
@@ -1,4 +1,3 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/GeoModelCore-GeoGenericFunctions.cmake)
 include(${SELF_DIR}/GeoModelCore-GeoModelKernel.cmake)
-include(${SELF_DIR}/GeoModelCore-GeoModelUtilities.cmake)

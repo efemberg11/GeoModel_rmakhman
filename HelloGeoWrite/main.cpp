6467baabaf6ccafc49f685e443efc6656a4c35b4
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2019-11-16 02:20:29 +0100

    Revert previous commit to restore units in the example.
    This reverts commit 3a4fa222629996b64e861f9dfac7e33c91de087e.

--- HelloGeoWrite/main.cpp
+++ HelloGeoWrite/main.cpp
@@ -9,7 +9,6 @@
 #include "GeoModelKernel/GeoPhysVol.h"
 #include "GeoModelKernel/GeoFullPhysVol.h"
 #include "GeoModelKernel/GeoNameTag.h"
-#include "GeoModelKernel/GeoElement.h"
 
 #include "GeoModelDBManager/GMDBManager.h"
 
@@ -28,42 +27,48 @@
 int main(int argc, char *argv[])
 {
   //-----------------------------------------------------------------------------------//
-	// Get the materials that we shall use.                                              //
+	// Define the materials that we shall use.                                              //
 	// ----------------------------------------------------------------------------------//
 
-	// Bogus densities.  Later: read from database.
-	double densityOfAir=0.1, densityOfAl=2.7;
-	
-	GeoElement * hydrogen=new GeoElement("Hydrogen"  , "H"    ,1,1.00797);       
-	GeoElement * deuterium=new GeoElement("Deuterium" , "D "   ,1,2.01);            
-	GeoElement * helium=new GeoElement("Helium"    , "He"   ,2,4.0026);   
-	GeoElement * lithium=new GeoElement("Lithium"   , "Li"   ,3,6.9410);  
-	GeoElement * beryllium=new GeoElement("Beryllium" , "Be"   ,4,9.0122);  
-	GeoElement * boron10=new GeoElement("Boron10"   , "B10"  ,5,10.0);    
-	GeoElement * boron11=new GeoElement("Boron11"   , "B11"  ,5,11.0);    
-	GeoElement * carbon=new GeoElement("Carbon"    , "C"    ,6,12.011); 
-	GeoElement * nitrogen=new GeoElement("Nitrogen"  , "N"    ,7,14.0067); 
-	GeoElement * oxygen=new GeoElement("Oxygen"    , "O"    ,8,15.9995 ); 
-	GeoElement * fluorine=new GeoElement("Fluorine"  , "F"    ,9,18.9984);
-	GeoElement * neon=new GeoElement("Neon"      , "Ne"   ,10,20.179);  
-	GeoElement * sodium=new GeoElement("Sodium"    , "Na"   ,11,22.9898);
-	GeoElement * magnesium=new GeoElement("Magnesium" , "Mg"   ,12,24.305);  
-	GeoElement * aluminium=new GeoElement("Aluminum"  , "Al"   ,13,26.9815); 
-	GeoElement * silicon=new GeoElement("Silicon"   , "Si"   ,14,28.086);  
-	
-	GeoMaterial *air        = new GeoMaterial("Air",densityOfAir);
-	air->add(nitrogen,0.79);
-	air->add(oxygen,0.21);
+	// Define the units
+	#define gr   SYSTEM_OF_UNITS::gram
+	#define mole SYSTEM_OF_UNITS::mole
+	#define cm3  SYSTEM_OF_UNITS::cm3
+
+	// Define the chemical elements
+	GeoElement*  Nitrogen = new GeoElement ("Nitrogen" ,"N"  ,  7.0 ,  14.0067 *gr/mole);
+	GeoElement*  Oxygen   = new GeoElement ("Oxygen"   ,"O"  ,  8.0 ,  15.9995 *gr/mole);
+	GeoElement*  Argon    = new GeoElement ("Argon"    ,"Ar" , 18.0 ,  39.948  *gr/mole);
+	GeoElement*  Hydrogen = new GeoElement ("Hydrogen" ,"H"  ,  1.0 ,  1.00797 *gr/mole);
+	GeoElement*  Iron     = new GeoElement ("Iron"     ,"Fe" , 26.0 ,  55.847  *gr/mole);
+	GeoElement*  Carbon   = new GeoElement ("Carbon"   ,"C"  ,  6.0 ,  12.0107 *gr/mole);
+	GeoElement*  Sillicon = new GeoElement ("Silicon"  ,"Si" , 14.0 ,  28.085  *gr/mole);
+
+	// Define the materials
+
+	double densityOfAir=0.001214 *gr/cm3;
+	GeoMaterial *air = new GeoMaterial("Air", densityOfAir);
+	air->add(Nitrogen  , 0.7494);
+	air->add(Oxygen, 0.2369);
+	air->add(Argon, 0.0129);
+	air->add(Hydrogen, 0.0008);
 	air->lock();
-	GeoMaterial *aluminum       = new GeoMaterial("aluminum",densityOfAl);
-	aluminum->add(aluminium,1.0);
-	aluminum->lock();
+
+	GeoMaterial* steel  = new GeoMaterial("Steel", 7.9 *gr/cm3);
+	steel->add(Iron  , 0.98);
+	steel->add(Carbon, 0.02);
+	steel->lock();
+
+	// Silicon 100% (Detector)
+	GeoMaterial* silicon = new GeoMaterial("Silicon", 2.329 *gr/cm3);
+	silicon->add(const_cast<GeoElement*> (Sillicon), 1.0);
+	silicon->lock();
+
 
   //-----------------------------------------------------------------------------------//
   // create the world volume container and
   // get the 'world' volume, i.e. the root volume of the GeoModel tree
   std::cout << "Creating the 'world' volume, i.e. the root volume of the GeoModel tree..." << std::endl;
-  
   const GeoBox* worldBox = new GeoBox(1000*SYSTEM_OF_UNITS::cm, 1000*SYSTEM_OF_UNITS::cm, 1000*SYSTEM_OF_UNITS::cm);
   const GeoLogVol* worldLog = new GeoLogVol("WorldLog", worldBox, air);
   GeoPhysVol* world = new GeoPhysVol(worldLog);
@@ -73,12 +78,12 @@ int main(int argc, char *argv[])
 	// Next make the box that describes the shape of the toy volume:                     //
 	const GeoBox      *toyBox    = new GeoBox(800*SYSTEM_OF_UNITS::cm, 800*SYSTEM_OF_UNITS::cm, 1000*SYSTEM_OF_UNITS::cm);                   //
 	// Bundle this with a material into a logical volume:                                //
-	const GeoLogVol   *toyLog    = new GeoLogVol("ToyLog", toyBox, air);                 //
+	const GeoLogVol   *toyLog    = new GeoLogVol("ToyLog", toyBox, silicon);                 //
 	// ..And create a physical volume:                                                   //
 	GeoPhysVol        *toyPhys   = new GeoPhysVol(toyLog);                               //
 
 	GeoBox       *sPass = new GeoBox(5.0*SYSTEM_OF_UNITS::cm, 30*SYSTEM_OF_UNITS::cm, 30*SYSTEM_OF_UNITS::cm);
-	GeoLogVol    *lPass = new GeoLogVol("Passive", sPass, aluminum);
+	GeoLogVol    *lPass = new GeoLogVol("Passive", sPass, steel);
 	GeoPhysVol   *pPass = new GeoPhysVol(lPass);
 
 	GeoBox       *sIPass = new GeoBox(4*SYSTEM_OF_UNITS::cm, 25*SYSTEM_OF_UNITS::cm, 25*SYSTEM_OF_UNITS::cm);
@@ -132,6 +137,8 @@ int main(int argc, char *argv[])
 
   std::cout << "\nTest - list of all the GeoMaterial nodes in the persistified geometry:" << std::endl;
   db.printAllMaterials();
+  std::cout << "\nTest - list of all the GeoElement nodes in the persistified geometry:" << std::endl;
+  db.printAllElements();
 
 
   return 0;

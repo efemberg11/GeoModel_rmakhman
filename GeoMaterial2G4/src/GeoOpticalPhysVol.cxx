403f1c7c20d9a34f67d8657e272b16fbab3e84d5
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2019-11-27 12:12:25 +0100

    Fix typo and include in the GeoOpticalPhysVol.cxx file

--- GeoMaterial2G4/src/GeoOpticalPhysVol.cxx
+++ GeoMaterial2G4/src/GeoOpticalPhysVol.cxx
@@ -1,8 +1,8 @@
-*
+/*
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
 
-#include "GeoModelUtilities/GeoOpticalPhysVol.h"
+#include "GeoMaterial2G4/GeoOpticalPhysVol.h"
 
 GeoOpticalPhysVol::GeoOpticalPhysVol(const GeoLogVol* LogVol):
   GeoPhysVol(LogVol)

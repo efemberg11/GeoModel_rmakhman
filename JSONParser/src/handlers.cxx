2436601ff6c1a9a5c02e7f629bec85717ff6a131
Andrea Dell'Acqua <andrea.dellacqua@cern.ch>
2020-02-01 15:06:28 -0500

    tidy up JSONParser

--- JSONParser/src/handlers.cxx
+++
@@ -1,8 +0,0 @@
-#include "JSONParser/tessellatedHandler.h"
-#include "JSONParser/pointsHandler.h"
-#include "JSONParser/facetsHandler.h"
-
-JSONHandler* jh1=handlerStore::createHandler<tessellatedHandler>("1_tessellated");
-JSONHandler* jh2=handlerStore::createHandler<pointsHandler>("2_Points");
-JSONHandler* jh3=handlerStore::createHandler<facetsHandler>("3_Facets");
-

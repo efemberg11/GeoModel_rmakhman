3d9865709f581030e7f0cddd1f7e10467f31eaa8
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-02-04 01:04:02 +0100

    Fix and modernize the CMake setup of the whole project.
    Each suproject had a CMake 'project' set up, which is not what we want. Also, many include dirs were explicitely defined, which is not needed in modern CMake.

---
+++ cmake/GeoModelVisualizationPackaging.cmake
@@ -0,0 +1,34 @@
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+
+# Set up the basic properties of the package(s).
+set( CPACK_PACKAGE_DESCRIPTION_SUMMARY
+   "${PROJECT_NAME} - ${PROJECT_VERSION}" )
+set( CPACK_PACKAGE_DESCRIPTION
+"Geometry model visualization libraries" )
+set( CPACK_PACKAGE_VERSION "${PROJECT_VERSION}" )
+set( CPACK_PACKAGE_CONTACT "atlas-sw-core@cern.ch" ) #TODO: change that
+
+# Set up the readme and license for the package.
+set( CPACK_RESOURCE_FILE_LICENSE ${CMAKE_SOURCE_DIR}/LICENSE )
+set( CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_SOURCE_DIR}/README.md )
+set( CPACK_RESOURCE_FILE_README ${CMAKE_SOURCE_DIR}/README.md )
+
+# Set up the handling of the Runtime and Development components during
+# the packaging.
+include( CPackComponent )
+set( CPACK_COMPONENTS_ALL Runtime Development )
+set( CPACK_RPM_COMPONENT_INSTALL TRUE )
+set( CPACK_DEB_COMPONENT_INSTALL TRUE )
+set( CPACK_COMPONENTS_IGNORE_GROUPS TRUE )
+cpack_add_component( Runtime
+   DISPLAY_NAME "GeoModelVisualization libraries"
+   DESCRIPTION "Runtime libraries of GeoModelVisualization"
+   REQUIRED )
+cpack_add_component( Development
+   DISPLAY_NAME "GeoModelVisualization development"
+   DESCRIPTION "Development package for GeoModelVisualization"
+   DEPENDS Runtime
+   DISABLED )
+
+# Include the main CPack configuration.
+include( CPack )

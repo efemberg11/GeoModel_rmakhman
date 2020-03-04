c67b51e7068ceab9da117fb521fa5cdb941c09c4
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2020-03-04 19:08:35 +0100

    Quasi-original copy of ATLAS magnetic field

---
+++ MagneticField/MagFieldServices/src/components/MagFieldServices_entries.cxx
@@ -0,0 +1,6 @@
+#include "MagFieldServices/AtlasFieldSvc.h"
+#include "MagFieldServices/H8FieldSvc.h"
+
+DECLARE_COMPONENT( MagField::AtlasFieldSvc )
+DECLARE_COMPONENT( MagField::H8FieldSvc )
+
---
+++ MagneticField/MagFieldUtils/src/components/MagFieldUtils_entries.cxx
@@ -0,0 +1,8 @@
+#include "MagFieldUtils/MagFieldTestbedAlg.h"
+#include "MagFieldUtils/SolenoidTest.h"
+#include "MagFieldUtils/IdentityManipulator.h"
+
+DECLARE_COMPONENT( MagField::MagFieldTestbedAlg )
+DECLARE_COMPONENT( MagField::SolenoidTest )
+DECLARE_COMPONENT( MagField::IdentityManipulator )
+

49c54c7cce170f24ceb4c98b559d8cfe359a1610
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2020-03-12 11:52:04 +0100

    Updated the project's build configuration to be in sync with the other GeoModel projects.
    This included moving all the common configuration options into the
    project configuration file, exporting all components in the
    "GeoModelTools::" namespace, setting up a semi-proper CPack
    configuration for the project, and cleaning the configuration
    as much as possible.

--- cmake/GeoModelToolsConfig.cmake
+++
@@ -1,5 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/GeoModelTools-ExpressionEvaluator.cmake)
-include(${SELF_DIR}/GeoModelTools-JSONParser.cmake)
-include(${SELF_DIR}/GeoModelTools-XMLParser.cmake)
-

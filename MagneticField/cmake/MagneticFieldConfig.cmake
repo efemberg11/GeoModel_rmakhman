5d3c52037232f77ba556159d7d4b96e827fffa40
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2020-05-18 15:51:41 +0200

    Add MagneticField/cmake/MagneticFieldConfig.cmake file

---
+++ MagneticField/cmake/MagneticFieldConfig.cmake
@@ -0,0 +1,5 @@
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+include(${SELF_DIR}/MagneticField-MagFieldInterfaces.cmake)
+include(${SELF_DIR}/MagneticField-MagFieldServices.cmake)
+

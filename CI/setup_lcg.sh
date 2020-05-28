be8f776731ce1eafc6c1d60ecc2b4d5bf0028560
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-05-28 10:55:41 +0000

    CI: update compiler versions both for CC7 and SLC6. Fixes the SLC6 CI build.

--- CI/setup_lcg.sh
+++ CI/setup_lcg.sh
@@ -5,10 +5,10 @@
 # determine os release
 if [ "$(cat /etc/redhat-release | grep 'Scientific Linux CERN SLC release 6')" ]; then
   os=slc6
-  compiler=gcc62-opt
+  compiler=gcc92-opt
 elif [ "$(cat /etc/centos-release | grep 'CentOS Linux release 7')" ]; then
   os=centos7
-  compiler=gcc8-opt
+  compiler=gcc92-opt
 else
   echo "Unknown OS" 1>&2
   exit 1

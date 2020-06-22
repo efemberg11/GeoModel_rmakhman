77dab5b07197285f83249f27baa576919cf07b46
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-06-22 10:27:08 +0200

    First working setup, with switch to optionally build gmex

--- cmake/PrintBuildInfo.cmake
+++ cmake/PrintBuildInfo.cmake
@@ -1,3 +1,5 @@
+# Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
+
 message(STATUS "-----")
 message(STATUS "Building with type: ${CMAKE_BUILD_TYPE}")
 message(STATUS "Using C++ standard: ${CMAKE_CXX_STANDARD}")
--- dependencies/FindEigen3.cmake
+++ dependencies/FindEigen3.cmake
@@ -10,7 +10,7 @@
 #  EIGEN3_INCLUDE_DIR - the eigen include directory
 #  EIGEN3_VERSION - eigen version
 #
-# This module reads hints about search locations from 
+# This module reads hints about search locations from
 # the following enviroment variables:
 #
 # EIGEN3_ROOT
@@ -75,7 +75,7 @@ else (EIGEN3_INCLUDE_DIR)
   if(NOT EIGEN3_INCLUDE_DIR)
     find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
         HINTS
-        ENV EIGEN3_ROOT 
+        ENV EIGEN3_ROOT
         ENV EIGEN3_ROOT_DIR
         PATHS
         ${CMAKE_INSTALL_PREFIX}/include

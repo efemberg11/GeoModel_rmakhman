25cdb29ae178b522e4a7c59a7521c0f72fede69b
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-06-26 11:46:31 +0200

    Add color definitions for CMake messages.

--- cmake/PrintBuildInfo.cmake
+++ cmake/PrintBuildInfo.cmake
@@ -1,6 +1,13 @@
 # Copyright (C) 2002-2020 CERN for the benefit of the ATLAS collaboration
 
-message(STATUS "-----")
-message(STATUS "Building with type: ${CMAKE_BUILD_TYPE}")
-message(STATUS "Using C++ standard: ${CMAKE_CXX_STANDARD}")
-message(STATUS "-----")
+if( COLOR_DEFS )
+  message(STATUS "-----")
+  message(STATUS "${Blue}Building with type: ${CMAKE_BUILD_TYPE}${ColourReset}")
+  message(STATUS "${Blue}Using C++ standard: ${CMAKE_CXX_STANDARD}${ColourReset}")
+  message(STATUS "-----")
+else()
+  message(STATUS "-----")
+  message(STATUS "Building with type: ${CMAKE_BUILD_TYPE}")
+  message(STATUS "Using C++ standard: ${CMAKE_CXX_STANDARD}")
+  message(STATUS "-----")
+endif()

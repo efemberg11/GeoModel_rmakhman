da809a440f09eead60ecf9ecee892fc96e46dca6
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-09-14 11:44:38 +0200

    empty definitions of constructor/destructor are now in header. Removing unnecessary cpp.

--- GeoModelCore/GeoModelKernel/GeoModelKernel/GeoVGeometryPlugin.h
+++ GeoModelCore/GeoModelKernel/GeoModelKernel/GeoVGeometryPlugin.h
@@ -13,22 +13,26 @@
  */
 
 #include "GeoModelKernel/GeoVPhysVol.h"
-//#include "GeoModelKernel/GeoVStore.h" // for create()
-#include "GeoModelKernel/GeoPublisher.h" // to publish lists of FullPhysVol and AlignableTransform nodes
+#include "GeoModelKernel/GeoVStore.h" // for create()
+#include "GeoModelKernel/GeoPublisher.h" // to publish lists of FullPhysVol and AlignableTransform nodes in create()
 
 class GeoPhysVol;
 
 class GeoVGeometryPlugin 
 {
  public:
-  GeoVGeometryPlugin() {};
-  GeoVGeometryPlugin(std::string name, GeoPublisher* publisher) : m_pluginName( name ), m_publisher(publisher) { m_publisher->setName(m_pluginName); } 
-  virtual ~GeoVGeometryPlugin();
 
-  // Create the system.
-  // Note: this is a pure virtual method, 
-  //       so you need to implement it in your derived plugin class
-  virtual void create (GeoPhysVol* world, GeoPublisher* publisher=nullptr) = 0;
+  //! Default constructor.
+  GeoVGeometryPlugin() {};
+  
+  //! Constructor used when publishing FullPhysVol/AlignableTransform nodes is needed.
+  GeoVGeometryPlugin(std::string name, GeoPublisher* publisher) : m_pluginName( name ), m_publisher(publisher) { m_publisher->setName(m_pluginName); } 
+  
+  virtual ~GeoVGeometryPlugin() {};
+
+  //! Create the system geometry.
+  // Note: this is a pure virtual method, so you need to implement it in your derived plugin class
+  virtual void create (GeoPhysVol* world, GeoPublisher* publisher = nullptr, GeoVStore* store = nullptr) = 0;
 
   // Get the plugin's name
   std::string getName() { return m_pluginName; };
@@ -41,12 +45,10 @@ class GeoVGeometryPlugin
   GeoVGeometryPlugin(const GeoVGeometryPlugin &right)=delete;
   GeoVGeometryPlugin & operator=(const GeoVGeometryPlugin &right)=delete;
 
-  // the name of the plugin. 
-  // It will be used to keep track of the FullPhysVol 
-  // and AlignableTransform nodes used in the plugin.
+  //! The name of the plugin, used to store plugin's published nodes. 
   std::string m_pluginName;
 
-  // a pointer to an instance of a class inheriting from GeoVStore
+  //! A GeoPublisher instance is used to publish lists of nodes.
   GeoPublisher* m_publisher;
 
 };

b23b6e9e5ac99f6941bfb42678819b112665cb4c
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-10-21 14:36:06 +0200

    Thanks to Attila, we fix the link against the imported target for macOS as well

--- dependencies/Dependencies.cmake
+++ dependencies/Dependencies.cmake
@@ -5,6 +5,14 @@ message(STATUS "Configuring external dependencies...")
 # Make this 'dependencies' module directory visible to CMake.
 list( APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/dependencies )
 
+# By default prefer not to use frameworks on macOS. 
+# But use it at a "LAST" resource, if no other options worked.
+# For details on the behaviour of this cache variable, see: 
+# - https://cmake.org/cmake/help/v3.0/command/find_file.html 
+set( CMAKE_FIND_FRAMEWORK "LAST" CACHE STRING
+   "Framework finding behaviour on macOS" )
+
+
 # Set up how the project handle some of the dependenices.
 include( SetupEigen3 )
 include( SetupXercesC )

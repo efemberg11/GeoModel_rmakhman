66133b62d813f1b860f9da001c0356765fad1bb8
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2020-12-08 13:11:54 +0100

    (cmake) Added global version for all GeoModel sub-projects

---
+++ cmake/GeoModel-version.cmake
@@ -0,0 +1,6 @@
+
+# Set up the version of GeoModel as a cache variable, so that other
+# sub-projects could use this value.
+set( GeoModel_VERSION "9.9.9" CACHE STRING
+    "Version of the GeoModel project" )
+

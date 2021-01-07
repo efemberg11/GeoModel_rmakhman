b8c2fd96e40dc8a21dbea93f699e3bbb1b37cc53
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2021-01-07 19:41:08 +0100

    (cmake) Move MuonDD to the new CMake setup

--- MuonDD/cmake/MuonDDConfig.cmake
+++
@@ -1,5 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/MuonDD-MuonAGDDDescription.cmake)
-include(${SELF_DIR}/MuonDD-MuonGeoModel.cmake)
-include(${SELF_DIR}/MuonDD-MuonAGDDBase.cmake)
-

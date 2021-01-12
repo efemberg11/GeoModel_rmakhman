b1d6c8dfed8619ea368a6abfb055700bd6327270
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2021-01-12 15:25:24 +0100

    Add scale and scale handler header files

---
+++ GeoModelTools/GDMLtoGM/GDMLInterface/scale.h
@@ -0,0 +1,14 @@
+#ifndef scale_H
+#define scale_H
+
+#include "GeoModelKernel/GeoDefinitions.h"
+
+struct scale {
+    scale():scalex(0),scaley(0),scalez(0) {;}
+	std::string name="";
+	double scalex=0;
+	double scaley=0;
+	double scalez=0;
+};
+
+#endif

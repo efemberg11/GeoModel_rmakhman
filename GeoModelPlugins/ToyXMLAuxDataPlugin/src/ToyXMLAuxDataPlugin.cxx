a8de6dd05dc1dff12c3f8aaf9ce4a9a6bc0f38dd
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2021-01-23 05:28:30 +0100

    Example Plugin saving to the DB auxiliary data from XML

--- GeoModelPlugins/ToyXMLAuxDataPlugin/src/ToyXMLAuxDataPlugin.cxx
+++ GeoModelPlugins/ToyXMLAuxDataPlugin/src/ToyXMLAuxDataPlugin.cxx
@@ -16,7 +16,7 @@
 #include "GeoModelKernel/GeoSerialDenominator.h"
 #include "GeoModelKernel/GeoAlignableTransform.h"
 #include "GeoModelKernel/GeoSerialTransformer.h"
-#include "GeoModelKernel/GeoPublisher.h" // to publish lists of FullPhysVol and AlignableTransform nodes
+#include "GeoModelKernel/GeoPublisher.h" // to publish lists of FullPhysVol and AlignableTransform nodes and auxiliary data
 
 #include "GeoGenericFunctions/AbsFunction.h"
 #include "GeoGenericFunctions/Variable.h"
@@ -24,6 +24,9 @@
 #include "GeoGenericFunctions/Cos.h"
 
 #include "GeoXmlMatManager/GeoXmlMatManager.h"
+#include "GeoXmlInpManager/GeoXmlInpManager.h"
+#include "GeoXmlInpManager/GeoInpRecordset.h"
+
 
 #include "GeoModelKernel/Units.h"
 #define SYSTEM_OF_UNITS GeoModelKernelUnits // so we will get, e.g., 'SYSTEM_OF_UNITS::cm'
@@ -34,15 +37,15 @@
 using namespace GeoGenfun;
 using namespace GeoXF;
 
-class ToyGeometryPlugin : public GeoVGeometryPlugin  {
+class ToyXMLAuxDataPlugin : public GeoVGeometryPlugin  {
 
  public:
 
   // Constructor 
-  ToyGeometryPlugin( std::string pluginName) : GeoVGeometryPlugin( pluginName ) {};
+  ToyXMLAuxDataPlugin( std::string pluginName) : GeoVGeometryPlugin( pluginName ) {};
 
   // Destructor:
-  ~ToyGeometryPlugin() {};
+  ~ToyXMLAuxDataPlugin() {};
 
   // Creation of geometry:
   virtual void create(GeoPhysVol *world, bool publish) override;
@@ -51,15 +54,15 @@ class ToyGeometryPlugin : public GeoVGeometryPlugin  {
  private:
 
   // Illegal operations:
-  const ToyGeometryPlugin & operator=(const ToyGeometryPlugin &right)=delete;
-  ToyGeometryPlugin(const ToyGeometryPlugin &right) = delete;
+  const ToyXMLAuxDataPlugin & operator=(const ToyXMLAuxDataPlugin &right)=delete;
+  ToyXMLAuxDataPlugin(const ToyXMLAuxDataPlugin &right) = delete;
  
 };
 
 
 
 //## Other Operations (implementation)
-void ToyGeometryPlugin::create(GeoPhysVol *world, bool publish)
+void ToyXMLAuxDataPlugin::create(GeoPhysVol *world, bool publish)
 {
   // Get the materials that we shall use.
   // -------------------------------------//
@@ -177,12 +180,61 @@ void ToyGeometryPlugin::create(GeoPhysVol *world, bool publish)
   world->add(tag);
   world->add(toyPhys);
   //--------------------------------------//
-  
+ 
+
+
+  //=====================
+  // Auxiliary Data
+  //=====================
+
+  // Now, we save auxiliary data to the output DB file
+  // in additional custom DB tables
+  // Those data are meant to be used by detector code later in the workflow
+  // so, it must be stored in the DB besides the actual GeoModel data themselves.
+  //
+
+  // In these first two examples, we load auxiliary data from custom XML files
+  // through the use of the GeoModelATLAS/GeoModelDataManagers
+  // Then, we store the auxiliary data directly to the DB files.
+  //
+  // At first, we need to get the GeoModelDataManagers' XML Input Manager
+  // As per GeoModelDataManagers' architecture, we need to load both the file
+  // containing the tables' defiintions and the one containing the actual tables' data
+  GeoXmlInpManager* inpman = GeoXmlInpManager::getManager();
+  inpman->parse("Hello-Defs.xml");
+  inpman->parse("Hello-Data.xml");
+  // Then, we set the name of the first of the tables that we want to store in the DB...
+  std::string tableName = "HelloCables";
+  // ...we get the corresponding "Recordset"'s data (i.e., the table's columns definitions and data rows
+  std::pair<std::map<std::string, std::vector<std::string>>, std::vector<std::vector<GeoInp>>> helloCablesData = inpman->getRecordsetData(tableName);
+  // ...and we store that table to the output DB
+  m_publisher->storeDataTable( tableName, (helloCablesData.first)["colNames"], (helloCablesData.first)["colTypes"], helloCablesData.second );
+
+  // Here an example for a second table of auxiliary data, which we want to store in the DB for later use
+  tableName = "HelloBoxes";
+  std::pair<std::map<std::string, std::vector<std::string>>, std::vector<std::vector<GeoInp>>> helloBoxesData = inpman->getRecordsetData(tableName);
+  m_publisher->storeDataTable( tableName, (helloBoxesData.first)["colNames"], (helloBoxesData.first)["colTypes"], helloBoxesData.second );
+
+  // Not all auxiliary data need to come from XML files.
+  // Here, we create a custom table made of strings, directly in the code.
+  // We define a table's name
+  // Then, we define the names and types of the columns, storing them as vectors of strings
+  // Then, we populate vectors of strings with the table's data. One vector for each table's row.
+  // Then, we store the data into the DB.
+  tableName = "HelloTest";
+  std::vector<std::string> tableColNames = {"MY_DATA_ID", "WEIGTH", "NAME"};
+  std::vector<std::string> tableColTypes = {"long", "double", "string"};
+  std::vector<std::variant<int,long,float,double,std::string>> vec1 = {1, 15, "Ped1"};
+  std::vector<std::variant<int,long,float,double,std::string>> vec2 = {3, "NULL", "Crate2"};
+  std::vector<std::vector<std::variant<int,long,float,double,std::string>>> tableData = { vec1, vec2 };
+  m_publisher->storeDataTable( tableName, tableColNames, tableColTypes, tableData );
+
+
   return;
 }
 
-extern "C" ToyGeometryPlugin *createToyGeometryPlugin() {
-  ToyGeometryPlugin* toy = new ToyGeometryPlugin( "ToyGeometryPlugin" );
+extern "C" ToyXMLAuxDataPlugin *createToyXMLAuxDataPlugin() {
+  ToyXMLAuxDataPlugin* toy = new ToyXMLAuxDataPlugin( "ToyXMLAuxDataPlugin" );
   std::cout << "The plugin, whose name is '" << toy->getName() << "', has been created." << std::endl;
   return toy;
 }

039fc99c393b1fb2951057da151d1954f37298b3
Andrea Dell'Acqua <andrea.dellacqua@cern.ch>
2021-02-05 11:43:05 +0100

    GDMLtoGM: added noisy printout in unidentifiedShapeHandler.cxx

--- GeoModelTools/GDMLtoGM/src/unidentifiedShapeHandler.cxx
+++ GeoModelTools/GDMLtoGM/src/unidentifiedShapeHandler.cxx
@@ -17,6 +17,11 @@ void unidentifiedShapeHandler::ElementHandle()
   std::string name=getAttributeAsString("name");
 
   //std::cout << "handling for box: name "<< name <<'\n';
+  
+  std::cout<<"\n***********************************************************************************\n\n"<<std::endl;
+  std::cout<<"Warning: this is unidentifiedShapeHandler called for "<<m_name<<" name "<<name<<"!!!"<<std::endl;
+  std::cout<<m_name<<" is not a supported shape yet!!"<<std::endl;
+  std::cout<<"\n***********************************************************************************\n\n"<<std::endl;
 
   GeoShape* unidentified=new GeoUnidentifiedShape(name);
   theController->saveSolid(name,unidentified);

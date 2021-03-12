e0cd47fa1e5511d8a1096d91c58bc5da576bef51
Joseph Francis Boudreau <boudreau@pitt.edu>
2021-03-12 13:17:59 -0500

    Added additional inputs to the Muon Plugin

--- GeoModelPlugins/MuonPlugin/src/MuonPlugin.cxx
+++ GeoModelPlugins/MuonPlugin/src/MuonPlugin.cxx
@@ -2,7 +2,19 @@
 
 #include "PluginTemplate.h"
 
-const std::vector<std::string> MuonPlugin::inputFiles={"Muons/Muons00.xml"};
+const std::vector<std::string> MuonPlugin::inputFiles={
+  "Muons/Muons00.xml",
+  "Muons/NSW/LM1.xml",
+  "Muons/NSW/LM2.xml",
+  "Muons/NSW/SM1.xml",
+  "Muons/NSW/SM2.xml",
+  "Muons/NSW/QL1.xml",
+  "Muons/NSW/QL2.xml",
+  "Muons/NSW/QL3.xml",
+  "Muons/NSW/QS1.xml",
+  "Muons/NSW/QS2.xml",
+  "Muons/NSW/QS3.xml"
+};
 extern "C" MuonPlugin *createMuonPlugin() {
   return new MuonPlugin;
 }

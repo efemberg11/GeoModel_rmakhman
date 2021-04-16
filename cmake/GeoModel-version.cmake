730042344dbfce38494d561920dd8c6dc0e1794d
Joseph Boudreau <joseph.boudreau@cern.ch>
2021-04-16 21:27:24 +0000

    Update GeoModel-version.cmake

--- cmake/GeoModel-version.cmake
+++ cmake/GeoModel-version.cmake
@@ -1,6 +1,5 @@
-
 # Set up the version of GeoModel as a cache variable, so that other
 # sub-projects could use this value.
-set( GeoModel_VERSION "4.1.1.1" CACHE STRING
+set( GeoModel_VERSION "4.2.0.0" CACHE STRING
     "Version of the GeoModel project" )
 

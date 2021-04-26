1ec67510f7fea124b262362c8c43b6c4e01b4661
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2021-04-26 16:42:03 +0200

    Removing un-needed folders from the GeoModelATLAS/GeoModelXML merge

--- AGDD/AGDDKernel/src/AGDDBuilder.cxx
+++
@@ -1,7 +0,0 @@
-/*
-  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
-*/
-
-#include "AGDDKernel/AGDDBuilder.h"
-
-AGDDBuilder* AGDDBuilder::s_theBuilder=0;
--- GeoModelPlugins/ITKPlugin/src/ITKPlugin.cxx
+++
@@ -1,8 +0,0 @@
-#define  PLUGINNAME ITKPlugin
-
-#include "PluginTemplate.h"
-
-const std::vector<std::string> ITKPlugin::inputFiles={"ITK/StripBlobRev_5433bf5.xml"};
-extern "C" ITKPlugin *createITKPlugin() {
-  return new ITKPlugin;
-}

d6c794ad6f2c89a6e6cb5c41c1a7fb5d81c007fd
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2021-11-19 14:33:12 -0800

    Protected m_delta in GeoAlignableTransform with a mutex

--- GeoModelCore/GeoModelKernel/src/GeoAlignableTransform.cxx
+++ GeoModelCore/GeoModelKernel/src/GeoAlignableTransform.cxx
@@ -29,20 +29,29 @@ __attribute__ ((flatten))
 #endif
 GeoTrf::Transform3D GeoAlignableTransform::getTransform(const GeoVAlignmentStore* store) const
 {
-  const GeoTrf::Transform3D* delta = (store==nullptr ? m_delta : store->getDelta(this));
-  return GeoTransform::getTransform(nullptr) * (delta==nullptr ? GeoTrf::Transform3D(GeoTrf::Transform3D::Identity()) : *delta);
+  if(store) {
+    const GeoTrf::Transform3D* delta = store->getDelta(this);
+    return GeoTransform::getTransform(nullptr) * (delta==nullptr ? GeoTrf::Transform3D(GeoTrf::Transform3D::Identity()) : *delta);
+  }
+  else {
+    std::scoped_lock<std::mutex> guard(m_deltaMutex);
+    return GeoTransform::getTransform(nullptr) * (m_delta==nullptr ? GeoTrf::Transform3D(GeoTrf::Transform3D::Identity()) : *m_delta);
+  }
 }
 
 void GeoAlignableTransform::setDelta (const GeoTrf::Transform3D& delta, GeoVAlignmentStore* store)
 {
   if(store==nullptr) {
-    if(m_delta && (m_delta->isApprox(delta))) return;
-    
-    if(m_delta) {
-      (*m_delta) = delta;
-    }
-    else {
-      m_delta = new GeoTrf::Transform3D(delta);
+    {
+      std::scoped_lock<std::mutex> guard(m_deltaMutex);
+      if(m_delta && (m_delta->isApprox(delta))) return;
+      
+      if(m_delta) {
+	(*m_delta) = delta;
+      }
+      else {
+	m_delta = new GeoTrf::Transform3D(delta);
+      }
     }
 
     std::set<GeoGraphNode*> uniqueParents;
@@ -63,9 +72,11 @@ void GeoAlignableTransform::clearDelta(GeoVAlignmentStore* store)
 {
   // Does not make sence to clear deltas inside Alignment Store
   if(store!=nullptr) return;
-
-  delete m_delta;
-  m_delta = nullptr;
+  {
+    std::scoped_lock<std::mutex> guard(m_deltaMutex);
+    delete m_delta;
+    m_delta = nullptr;
+  }
   
   std::set<GeoGraphNode*> uniqueParents;
   for(GeoGraphNode* parent : m_parentList) {

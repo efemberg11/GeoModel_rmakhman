cfc3d387afc97eaba12eebd555c0e5b1f7ef9b07
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2022-02-11 12:19:23 +0100

    Remove debug message from MyLengthIntegratorSteppingAction

--- FullSimLight/src/MyLengthIntegratorSteppingAction.cc
+++ FullSimLight/src/MyLengthIntegratorSteppingAction.cc
@@ -151,8 +151,8 @@ namespace G4UA
         double zHit = aStep->GetPreStepPoint()->GetPosition().z();
         double rHit = aStep->GetPreStepPoint()->GetPosition().perp();
         
-        std::cout<<"zHit: "<<zHit<<" ::["<<fGeantinoMapsConfig->GetZmin()<<","<<fGeantinoMapsConfig->GetZmax()<<"]"<<std::endl;
-        std::cout<<"rHit: "<<rHit<<" ::["<<fGeantinoMapsConfig->GetRmin()<<","<<fGeantinoMapsConfig->GetRmax()<<"]"<<std::endl;
+//        std::cout<<"zHit: "<<zHit<<" ::["<<fGeantinoMapsConfig->GetZmin()<<","<<fGeantinoMapsConfig->GetZmax()<<"]"<<std::endl;
+//        std::cout<<"rHit: "<<rHit<<" ::["<<fGeantinoMapsConfig->GetRmin()<<","<<fGeantinoMapsConfig->GetRmax()<<"]"<<std::endl;
         
         if(zHit >= fGeantinoMapsConfig->GetZmin() && zHit <= fGeantinoMapsConfig->GetZmax() && rHit >= fGeantinoMapsConfig->GetRmin() && rHit <= fGeantinoMapsConfig->GetRmax()){
             MyLengthIntegratorSteppingAction::addToDetThickMap(detName_d,            thickstepRL, thickstepIL);

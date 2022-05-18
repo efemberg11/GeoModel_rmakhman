52473436dac895fa2eb1e4b3273968bd1ed15376
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2022-05-18 16:13:40 +0200

    FullSimLight to read a json configuration file - and set geometry filename, nEvents and g4UI commands properly

--- FullSimLight/fullSimLight.cc
+++ FullSimLight/fullSimLight.cc
@@ -4,9 +4,9 @@
 
 #include "G4Types.hh"
 #ifdef G4MULTITHREADED
-  #include "G4MTRunManager.hh"
+#include "G4MTRunManager.hh"
 #else
-  #include "G4RunManager.hh"
+#include "G4RunManager.hh"
 #endif
 
 #include "G4UImanager.hh"
@@ -35,6 +35,9 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+#include <nlohmann/json.hpp>
+#include <fstream>
+
 static const std::string fullSimLightShareDir=FULLSIMLIGHTSHAREDIR;
 static std::string  parMacroFileName   = fullSimLightShareDir+"/macro.g4";
 static bool         parIsPerformance   = false;
@@ -42,69 +45,81 @@ static G4String     geometryFileName   ;
 static std::string  parPhysListName    = "FTFP_BERT";
 static bool         parRunOverlapCheck = false;
 bool isBatch = true;
+static std::string  parConfigFileName  = "config.json";
 
 void GetInputArguments(int argc, char** argv);
 void Help();
 
 
 int main(int argc, char** argv) {
-
-  // JFB if the G4 environment does not already set path to these variables, look for
-  // them in standard places.
-  
-  auto dataSetEnv=[] (const std::string &dir, const std::string & dataSetEnvName, const std::string & dataset) {
-    if (getenv(dataSetEnvName.c_str())) {
-      std::cout << dataSetEnvName << "=" << getenv(dataSetEnvName.c_str()) << std::endl; 
-    }
-    else {
-      DIR *directory = opendir(dir.c_str());
-      if (directory) {
-	dirent * entry = readdir(directory);
-	while (entry) {
-	  std::string entryName=entry->d_name;
-	  if (entryName.find(dataset)!=std::string::npos) {
-	    std::cout << dataSetEnvName << "=" << (dir+"/"+entryName) << std::endl; 
-	    setenv(dataSetEnvName.c_str(),(dir+"/"+entryName).c_str(),0);
-	  }
-	  entry=readdir(directory);
-	}
-	closedir(directory);
-      }
-    }
-  };
     
-  
-  const std::string g4ShareDir=G4SHAREDIR;
-  const std::string g4Version=G4VERSION;
-  const std::string searchDir=g4ShareDir+"/Geant4-"+g4Version+"/data";
-
-  dataSetEnv(searchDir,"G4NEUTRONHPDATA", "G4NDL");
-  dataSetEnv(searchDir,"G4LEDATA","G4EMLOW");
-  dataSetEnv(searchDir,"G4LEVELGAMMADATA","PhotonEvaporation");
-  dataSetEnv(searchDir,"G4RADIOACTIVEDATA","RadioactiveDecay");
-  dataSetEnv(searchDir,"G4PARTICLEXSDATA","G4PARTICLEXS");
-  dataSetEnv(searchDir,"G4PIIDATA","G4PII");
-  dataSetEnv(searchDir,"G4REALSURFACEDATA","RealSurface");
-  dataSetEnv(searchDir,"G4SAIDXSDATA","G4SAIDDATA");
-  dataSetEnv(searchDir,"G4ABLADATA","G4ABLA");
-  dataSetEnv(searchDir,"G4INCLDATA","G4INCL");
-  dataSetEnv(searchDir,"G4ENSDFSTATEDATA","G4ENSDFSTATE");
-  
+    // JFB if the G4 environment does not already set path to these variables, look for
+    // them in standard places.
+    
+    auto dataSetEnv=[] (const std::string &dir, const std::string & dataSetEnvName, const std::string & dataset) {
+        if (getenv(dataSetEnvName.c_str())) {
+            std::cout << dataSetEnvName << "=" << getenv(dataSetEnvName.c_str()) << std::endl;
+        }
+        else {
+            DIR *directory = opendir(dir.c_str());
+            if (directory) {
+                dirent * entry = readdir(directory);
+                while (entry) {
+                    std::string entryName=entry->d_name;
+                    if (entryName.find(dataset)!=std::string::npos) {
+                        std::cout << dataSetEnvName << "=" << (dir+"/"+entryName) << std::endl;
+                        setenv(dataSetEnvName.c_str(),(dir+"/"+entryName).c_str(),0);
+                    }
+                    entry=readdir(directory);
+                }
+                closedir(directory);
+            }
+        }
+    };
+    
+    
+    const std::string g4ShareDir=G4SHAREDIR;
+    const std::string g4Version=G4VERSION;
+    const std::string searchDir=g4ShareDir+"/Geant4-"+g4Version+"/data";
+    
+    
+    G4cout
+    << " =============== Running FullSimLight ================ "      << G4endl;
+    
     // Get input arguments
     GetInputArguments(argc, argv);
-
+    
+    if(isBatch){
+        G4cout
+        << "   Command line parameters"                                    << G4endl
+        << "   Physics list name  =   " << parPhysListName                 << G4endl
+        << "   Geant4 macro       =   " << parMacroFileName                << G4endl
+        << "   Performance mode   =   " << parIsPerformance                << G4endl
+        << "   Geometry file      =   " << geometryFileName                << G4endl
+        << "   Run Overlap Check  =   " << parRunOverlapCheck              << G4endl
+        << " ===================================================== \n"     << G4endl;
+        
+    }
+    
     G4cout
-    << " =============== Running FullSimLight ================ "      << G4endl
-    << "   Physics list name  =  " << parPhysListName                 << G4endl
-    << "   Geant4 macro       =  " << parMacroFileName                << G4endl
-    << "   Performance mode   =  " << parIsPerformance                << G4endl
-    << "   Geometry file      =  " << geometryFileName                << G4endl
-    << "   Run Overlap Check  =  " << parRunOverlapCheck              << G4endl
-    << " ===================================================== "      << G4endl;
-
+    << " == Geant4 Environment Variables == "      << G4endl;
+    dataSetEnv(searchDir,"G4NEUTRONHPDATA", "G4NDL");
+    dataSetEnv(searchDir,"G4LEDATA","G4EMLOW");
+    dataSetEnv(searchDir,"G4LEVELGAMMADATA","PhotonEvaporation");
+    dataSetEnv(searchDir,"G4RADIOACTIVEDATA","RadioactiveDecay");
+    dataSetEnv(searchDir,"G4PARTICLEXSDATA","G4PARTICLEXS");
+    dataSetEnv(searchDir,"G4PIIDATA","G4PII");
+    dataSetEnv(searchDir,"G4REALSURFACEDATA","RealSurface");
+    dataSetEnv(searchDir,"G4SAIDXSDATA","G4SAIDDATA");
+    dataSetEnv(searchDir,"G4ABLADATA","G4ABLA");
+    dataSetEnv(searchDir,"G4INCLDATA","G4INCL");
+    dataSetEnv(searchDir,"G4ENSDFSTATEDATA","G4ENSDFSTATE");
+    G4cout<< " ===================================================== " << G4endl;
+    
+    
     G4Timer FSLTotalCPUTimer;
     FSLTotalCPUTimer.Start();
-
+    
     //choose the Random engine: set to MixMax explicitely (default form 10.4)
     G4Random::setTheEngine(new CLHEP::MixMaxRng);
     // set seed and print info
@@ -115,7 +130,7 @@ int main(int argc, char** argv) {
     << "   Initial seed       = " << G4Random::getTheSeed()           << G4endl
     << " ===================================================== "      << G4endl
     << G4endl;
-
+    
     // Construct the default run manager
 #ifdef G4MULTITHREADED
     G4MTRunManager* runManager = new G4MTRunManager;
@@ -125,7 +140,7 @@ int main(int argc, char** argv) {
 #else
     G4RunManager* runManager = new G4RunManager;
 #endif
-
+    
     // 1. Physics list
     G4bool activateRegions = false;
     G4VModularPhysicsList* physList = nullptr;
@@ -153,23 +168,20 @@ int main(int argc, char** argv) {
     }
     // In cases of ATLAS physics lists, set the neutron tracking cut to be 150 [ns] as in Athena
     if (parPhysListName.find("ATL") != std::string::npos) {
-      G4NeutronTrackingCut* neutronCut = new G4NeutronTrackingCut("neutronCutphysics", 1);
-      neutronCut->SetTimeLimit(150.0*CLHEP::ns);
-      physList->ReplacePhysics(neutronCut);
+        G4NeutronTrackingCut* neutronCut = new G4NeutronTrackingCut("neutronCutphysics", 1);
+        neutronCut->SetTimeLimit(150.0*CLHEP::ns);
+        physList->ReplacePhysics(neutronCut);
     }
     
     // register the final version of the physics list in the run manager
     runManager->SetUserInitialization(physList);
-
+    
     // 2. Detector construction
     FSLDetectorConstruction* detector = new FSLDetectorConstruction;
-
+    
     if (parRunOverlapCheck) detector->SetRunOverlapCheck(true);
     if (activateRegions)    detector->SetAddRegions(true);
-
-    detector->SetGeometryFileName (geometryFileName);
-    runManager->SetUserInitialization(detector);
-
+    
     // 3. User action
     FSLActionInitialization* actInit = new FSLActionInitialization(parIsPerformance);
     // set the name of a region in which we are interested to see a very basic simulation
@@ -178,44 +190,83 @@ int main(int argc, char** argv) {
     const G4String nameSpecialScoringRegion = ""; //"EMEC"
     actInit->SetSpecialScoringRegionName(nameSpecialScoringRegion);
     runManager->SetUserInitialization(actInit);
-
+    
     // 4. Run the simulation in batch mode
     G4UImanager* UI = G4UImanager::GetUIpointer();
     
     if(isBatch){
+        
+        detector->SetGeometryFileName (geometryFileName);
+        runManager->SetUserInitialization(detector);
+        
         G4String command = "/control/execute ";
         UI->ApplyCommand(command+parMacroFileName);
         
     } else
     {
-        //It should instantiate the configurator that reads and parse the json configuration file
-        FSLConfigurator fsl_conf=FSLConfigurator();
-        // Hard coded string commands that will be taken and fed from the config files  
-        UI->ApplyCommand("/control/verbose 0");
-        UI->ApplyCommand("/run/verbose 0");
-        UI->ApplyCommand("/event/verbose 0");
-        UI->ApplyCommand("/tracking/verbose 0");
-        UI->ApplyCommand("/run/numberOfThreads 1");
-        UI->ApplyCommand("/control/cout/prefixString G4Worker_");
-        UI->ApplyCommand("/FSLdet/setField 4.0 tesla");
-        UI->ApplyCommand("/FSLgun/primaryPerEvt 2");
-        UI->ApplyCommand("/FSLgun/energy  10 GeV");
-        //UI->ApplyCommand("/FSLgun/direction  0 1 0");
-        UI->ApplyCommand("/process/list");
-
+        
+        std::ifstream ifs(parConfigFileName);
+        auto jf=json::parse(ifs);
+        
+        simConfig::fslConfig fsl;
+        simConfig::regionConfig rc;
+        
+        //read and store the configuration into the fslConfig struct
+        simConfig::from_json(jf, fsl);
+        
+        detector->SetGeometryFileName (fsl.geometry);
+        runManager->SetUserInitialization(detector);
+        
+        //parse RegionsData
+        for (const auto& element : jf["Regions data"]){
+            
+//            std::cout<<"RegionName: "<<element["RegionName"]<<std::endl;
+//            std::cout<<"RootLVNames: "<<element["RootLVNames"]<<std::endl;
+//            std::cout<<"GammaCut: "<<element["GammaCut"]<<std::endl;
+//            std::cout<<"ElectronCut: "<<element["ElectronCut"]<<std::endl;
+//            std::cout<<"PositronCut: "<<element["PositronCut"]<<std::endl;
+//            std::cout<<"ProtonCut: "<<element["ProtonCut"]<<std::endl;
+            
+            //actually read the data and save them in a simConfig::regionConfig object (might be useful for the configuration later on)
+            simConfig::from_json(element, rc);
+//            std::cout<<"RegionName: "<<rc.regionName<<std::endl;
+//            std::cout<<"RootLVNames size: "<<rc.rootLVNames.size()<<std::endl;
+//            std::cout<<"GammaCut: "<<rc.gammaCut<<std::endl;
+//            std::cout<<"ElectronCut: "<<rc.electronCut<<std::endl;
+//            std::cout<<"PositronCut: "<<rc.positronCut<<std::endl;
+//            std::cout<<"ProtonCut: "<<rc.protonCut<<std::endl;
+//            std::cout<<"------------------------------------------------"<<std::endl;
+        }
+        
+        //parse and apply G4Commands
+        for (const auto& element : jf["g4ui_commands"]){
+            //std::cout<<"G4Commands: "<<element<<std::endl;
+            UI->ApplyCommand(element);
+        }
+        
+        G4cout
+        << " ===================================================== "      << G4endl
+        << "   Configuration parameters"                                  << G4endl
+        << "   Geometry file      =  " << fsl.geometry                    << G4endl
+        << "   Physics list name  =  " << fsl.physicsList                 << G4endl
+        << "   Generator          =  " << fsl.eventGeneratorName          << G4endl
+        << "   Magnetic Field     =  " << fsl.magFieldIntensity           << G4endl
+        << "   Run Overlap Check  =  " << parRunOverlapCheck              << G4endl
+        << " ===================================================== "      << G4endl;
+        
+        
         // Initialize G4 kernel
         runManager->Initialize();
-        int numberOfEvent = 10;
-        runManager->BeamOn(numberOfEvent);
+        runManager->BeamOn(fsl.nEvents);
     }
-
+    
     //
     // Print out the final random number
     G4cout << G4endl
-           << " ================================================================= " << G4endl
-           << " Final random number = " << CLHEP::HepRandom::getTheEngine()->flat() << G4endl
-      	   << " ================================================================= " << G4endl
-           << G4endl;
+    << " ================================================================= " << G4endl
+    << " Final random number = " << CLHEP::HepRandom::getTheEngine()->flat() << G4endl
+    << " ================================================================= " << G4endl
+    << G4endl;
     //
     // Delete the RunManager
     delete runManager;
@@ -230,6 +281,7 @@ int main(int argc, char** argv) {
 
 static struct option options[] = {
     {"physics list name        "  , required_argument, 0, 'f'},
+    {"configuration file       "  , required_argument, 0, 'c'},
     {"macro file               "  , required_argument, 0, 'm'},
     {"performance flag         "  , no_argument      , 0, 'p'},
     {"geometry file name       "  , required_argument, 0, 'g'},
@@ -241,83 +293,82 @@ static struct option options[] = {
 
 
 void Help() {
-  std::cout <<"\n " << std::setw(100) << std::setfill('=') << "" << std::setfill(' ') << std::endl;
-  G4cout <<"  FullSimLight Geant4 application.    \n"
-            << std::endl
-            <<"  **** Parameters: \n\n"
-            <<"      -g :   REQUIRED : the Geometry file name \n"
-            <<"      -m :   the standard Geant4 macro file name \n"
-            <<"      -o :   flag  ==> run the geometry overlap check (default: FALSE)\n"
-            <<"      -f :   physics list name (default: FTFP_BERT) \n"
-            <<"      -P :   use Pythia primary generator [config. available: ttbar/higgs/minbias or use a Pythia command input file]\n"
-            <<"      -p :   flag  ==> run the application in performance mode i.e. no user actions \n"
-            <<"         :   -     ==> run the application in NON performance mode i.e. with user actions (default) \n"<< std::endl;
-
-  std::cout <<"\nUsage: ./fullSimLight [OPTIONS] -m <MACRO_FILE>\n" <<std::endl;
-  for (int i=0; options[i].name!=NULL; i++) {
-    //printf("\t-%c  --%s\t%s\n", options[i].val, options[i].name, options[i].has_arg ? options[i].name : "");
-    printf("\t-%c  --%s\t\n", options[i].val, options[i].name);
-  }
-  std::cout<<"\n "<<std::setw(100)<<std::setfill('=')<<""<<std::setfill(' ')<<std::endl;
+    std::cout <<"\n " << std::setw(100) << std::setfill('=') << "" << std::setfill(' ') << std::endl;
+    G4cout <<"  FullSimLight Geant4 application.    \n"
+    << std::endl
+    <<"  ** Please specify a json configuration file with the -c flag or use the following command line parameters : \n\n"
+    <<"  **** Command line parameters: \n\n"
+    <<"      -g :   REQUIRED : the Geometry file name \n"
+    <<"      -m :   the Geant4 macro file name \n"
+    <<"      -o :   flag  ==> run the geometry overlap check (default: FALSE)\n"
+    <<"      -f :   physics list name (default: FTFP_BERT) \n"
+    <<"      -P :   use Pythia primary generator [config. available: ttbar/higgs/minbias or use a Pythia command input file]\n"
+    <<"      -p :   flag  ==> run the application in performance mode i.e. no user actions \n"
+    <<"         :   -     ==> run the application in NON performance mode i.e. with user actions (default) \n"<< std::endl;
+    
+    std::cout <<"\nUsage: ./fullSimLight [OPTIONS] -m <MACRO_FILE>\n" <<std::endl;
+    for (int i=0; options[i].name!=NULL; i++) {
+        //printf("\t-%c  --%s\t%s\n", options[i].val, options[i].name, options[i].has_arg ? options[i].name : "");
+        printf("\t-%c  --%s\t\n", options[i].val, options[i].name);
+    }
+    std::cout<<"\n "<<std::setw(100)<<std::setfill('=')<<""<<std::setfill(' ')<<std::endl;
 }
 
 void GetInputArguments(int argc, char** argv) {
-  // process arguments
-  if (argc == 1) {
-   Help();
-   exit(0);
-  }
-  while (true) {
-   int c, optidx = 0;
-   c = getopt_long(argc, argv, "P:pm:f:g:oh", options, &optidx);
-   if (c == -1)
-     break;
-   //
-   switch (c) {
-   case 0:
-     c = options[optidx].val;
-     break;
-   case 'P':
+    // process arguments
+    if (argc == 1) {
+        Help();
+        exit(0);
+    }
+    while (true) {
+        int c, optidx = 0;
+        c = getopt_long(argc, argv, "P:pm:f:g:c:oh", options, &optidx);
+        if (c == -1)
+            break;
+        //
+        switch (c) {
+            case 0:
+                c = options[optidx].val;
+                break;
+            case 'P':
 #if USE_PYTHIA
-     set_pythia_config(optarg);
+                set_pythia_config(optarg);
 #else
-     std::cerr << "Support for Pythia is not available. \nPlease visit the website http://home.thep.lu.se/Pythia/ to install it in your system." << std::endl;
-     exit(1);
+                std::cerr << "Support for Pythia is not available. \nPlease visit the website http://home.thep.lu.se/Pythia/ to install it in your system." << std::endl;
+                exit(1);
 #endif
-     break;
-   case 'p':
-     parIsPerformance = true;
-     break;
-   case 'm':
-     parMacroFileName = optarg;
-     break;
-   case 'f':
-     parPhysListName  = optarg;
-     break;
-   case 'g':
-     geometryFileName = optarg;
-     break;
-   case 'o':
-     parRunOverlapCheck = true;
-     break;
-   case 'h':
-     Help();
-     exit(0);
-   default:
-     Help();
-     errx(1, "unknown option %c", c);
-   }
-  }
-  // check if mandatory Geant4 macro file was provided
-  if (parMacroFileName=="") {
-      G4cout << "  *** ERROR : Geant4 macro file is required. " << G4endl;
-      Help();
-      exit(-1);
-  }
-  // check if mandatory Geometry file was provided
-  if (geometryFileName=="") {
-      G4cout << "  *** ERROR : Geometry file is required. " << G4endl;
-      Help();
-      exit(-1);
+                break;
+            case 'p':
+                parIsPerformance = true;
+                break;
+            case 'm':
+                parMacroFileName = optarg;
+                break;
+            case 'f':
+                parPhysListName  = optarg;
+                break;
+            case 'g':
+                geometryFileName = optarg;
+                break;
+            case 'c':
+                isBatch = false;
+                parConfigFileName = optarg;
+                break;
+            case 'o':
+                parRunOverlapCheck = true;
+                break;
+            case 'h':
+                Help();
+                exit(0);
+            default:
+                Help();
+                errx(1, "unknown option %c", c);
+        }
+    }
+    // check if mandatory Geometry file was provided
+    if (geometryFileName=="" && isBatch) {
+        G4cout << "  *** ERROR : Geometry file is required. Please provide it in a json configuration file or via command line using the -g flag " << G4endl;
+        Help();
+        exit(-1);
     }
 }
--- FullSimLight/include/FSLConfigurator.hh
+++ FullSimLight/include/FSLConfigurator.hh
@@ -25,20 +25,92 @@ using json = nlohmann::json;
  *
  */
 
-class FSLConfigurator {
-    
-public:
-    
-    FSLConfigurator();
-    ~FSLConfigurator(){};
-    FSLConfigurator(const FSLConfigurator&) = delete;
-    FSLConfigurator& operator=(const FSLConfigurator&) = delete;
-    
-    
+namespace simConfig {
 
-private:
-  
+struct regionConfig {
+    std::string   regionName;
+    std::vector<std::string> rootLVNames;
+    double  gammaCut;
+    double  electronCut;
+    double  positronCut;
+    double  protonCut;
+};
+
+struct fslConfig{
     
-}; // FSLConfigurator
+    std::string geometry;
+    std::string physicsList;
+    int nEvents;
+    
+    std::string eventGeneratorName;
+    std::string eventInputFile;
+    std::string typeOfEvent;
+    
+    std::vector<std::string> sensitiveDetectors;
+    std::string outputHitsFile;
+    std::string outputHistoFile;
+    
+    std::vector<regionConfig> regionsData;
+    
+    std::string magFieldIntensity;
+    std::string magFieldMap;
+    std::string magFieldPlugin;
+    
+    std::vector<std::string> userActionExtensions;
+    std::vector<std::string> g4UiCommands;
+};
+
+inline void to_json(json& j, const fslConfig& p) {
+    j = json{{"Geometry", p.geometry},{"Physics list name", p.physicsList},{"Number of events", p.nEvents},{"Magnetic field intensity", p.magFieldIntensity},{"Generator", p.eventGeneratorName},{"Event input file", p.eventInputFile},{"Type of event", p.typeOfEvent},{"Sensitive Detector Extensions", p.sensitiveDetectors},{"Output Hits file", p.outputHitsFile},{"Output Histo file", p.outputHistoFile},{"Magnetic Field Map", p.magFieldMap},{"Magnetic Field Plugin", p.magFieldPlugin},{"User Action Extensions", p.userActionExtensions},{"g4ui_commands", p.g4UiCommands}};
+    
+}
+inline void to_json(json& j, const regionConfig& r) {
+    j = json{{"RegionName", r.regionName},{"RootLVNames", r.rootLVNames},{"GammaCut", r.gammaCut},{"ElectronCut", r.electronCut},{"PositronCut", r.positronCut},{"ProtonCut", r.protonCut}};
+}
+inline void from_json(const json& j, fslConfig& p) {
+    p.geometry=j.at("Geometry").get<std::string>();
+    p.physicsList=j.at("Physics list name").get<std::string>();
+    p.nEvents=j.at("Number of events").get<int>();
+    p.magFieldIntensity=j.at("Magnetic field intensity").get<std::string>();
+    p.eventGeneratorName=j.at("Generator").get<std::string>();
+    p.eventInputFile=j.at("Event input file").get<std::string>();
+    p.typeOfEvent=j.at("Type of event").get<std::string>();
+    p.sensitiveDetectors=j.at("Sensitive Detector Extensions").get<std::vector<std::string>>();
+    p.outputHitsFile=j.at("Output Hits file").get<std::string>();
+    p.outputHistoFile=j.at("Output Histo file").get<std::string>();
+    p.magFieldMap=j.at("Magnetic Field Map").get<std::string>();
+    p.magFieldPlugin=j.at("Magnetic Field Plugin").get<std::string>();
+    p.userActionExtensions=j.at("User Action Extensions").get<std::vector<std::string>>();
+    p.g4UiCommands=j.at("g4ui_commands").get<std::vector<std::string>>();
+    
+}
+
+inline void from_json(const json& j, regionConfig& r) {
+    r.regionName=j.at("RegionName").get<std::string>();
+    r.rootLVNames=j.at("RootLVNames").get<std::vector<std::string>>();
+    r.gammaCut=j.at("GammaCut").get<double>();
+    r.electronCut=j.at("ElectronCut").get<double>();
+    r.positronCut=j.at("PositronCut").get<double>();
+    r.protonCut=j.at("ProtonCut").get<double>();
+
+}
+
+} // namespace fslconf
+
+//class FSLConfigurator {
+//
+//public:
+//
+//    FSLConfigurator(){};
+//    ~FSLConfigurator(){};
+//    FSLConfigurator(const FSLConfigurator&) = delete;
+//    FSLConfigurator& operator=(const FSLConfigurator&) = delete;
+//
+//
+//
+//private:
+//
+//    simConfig::fslConfig fSimConfigurator;
+//}; // FSLConfigurator
 
 #endif // FSLConfigurator_h 1

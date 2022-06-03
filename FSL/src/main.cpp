247e8588717563f2a2b8fc599c6df0cd79566c11
Raees Khan <rak177@pitt.edu>
2022-06-03 04:16:51 -0400

    Adding auto scaling to config utility

--- FSL/src/main.cpp
+++ FSL/src/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication a(argc, argv);
     FSLMainWindow w;
     w.show();
     return a.exec();

4c4d2a80b5d85b80994aa6b2f4d9cba9eb00b0eb
Vakho Tsulaia <vakhtang.tsulaia@cern.ch>
2022-09-14 16:36:26 -0700

    Removed printouts from GeoModelKernel/GeoVolumeTagCatalog.h

--- GeoModelCore/GeoModelKernel/GeoModelKernel/GeoVolumeTagCatalog.h
+++ GeoModelCore/GeoModelKernel/GeoModelKernel/GeoVolumeTagCatalog.h
@@ -11,13 +11,10 @@ class GeoVolumeTagCatalog {
 public:
 	void addTaggedVolume(std::string category, std::string tag, GeoVPhysVol* v)
 	{
-		std::cout << "adding volume "<< v->getLogVol()->getName()<<" to category "<<category<<" with tag "<<tag<<std::endl;
 		theTags[category][tag]=v;	
 	} 
 	GeoVPhysVol* getTaggedVolume(std::string category, std::string tag)
 	{
-		std::cout << "getting volume "<< " with tag "<<tag<<" from category "<<category<<std::endl;
-		std::cout<<" getting volume "<<tag<<" pointer "<<theTags[category][tag]<<std::endl;
 		return theTags[category][tag];
 	}
 

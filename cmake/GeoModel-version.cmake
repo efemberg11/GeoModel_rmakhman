fd9c6b8ef938f2848574c1a46a9af6953eb35397
Joseph F. Boudreau <boudreau@pitt.edu>
2023-05-08 05:29:39 -0500

    update to new ATLAS Extensions (with improved error message for missing magnetic field map, and bump the version number

--- cmake/GeoModel-version.cmake
+++ cmake/GeoModel-version.cmake
@@ -1,5 +1,5 @@
 # Set up the version of GeoModel as a cache variable, so that other
 # sub-projects could use this value.
-set( GeoModel_VERSION "4.4.3" CACHE STRING
+set( GeoModel_VERSION "4.4.4" CACHE STRING
     "Version of the GeoModel project" )
 

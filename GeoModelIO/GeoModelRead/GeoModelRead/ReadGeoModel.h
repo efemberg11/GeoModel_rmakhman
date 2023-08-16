cb776f164bd3653cf3c07e4ce6254959829f09da
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2023-08-16 19:06:20 +0200

    code indenting

--- GeoModelIO/GeoModelRead/GeoModelRead/ReadGeoModel.h
+++ GeoModelIO/GeoModelRead/GeoModelRead/ReadGeoModel.h
@@ -100,10 +100,12 @@ class ReadGeoModel {
 
     GeoPhysVol* buildGeoModel();
 
-    //NB, this template method needs only the "publisher name" to be specified (i.e. the last suffix), since
-    //the first part of the table name get added automatically according to the data type it is templated on
+    // NB, this template method needs only the "publisher name" to be specified
+    // (i.e. the last suffix), since the first part of the table name get added
+    // automatically according to the data type it is templated on
     template <typename T, class N>
-    std::map<T, N> getPublishedNodes(std::string publisherName = "", bool doCheckTable = false);
+    std::map<T, N> getPublishedNodes(std::string publisherName = "",
+                                     bool doCheckTable = false);
 
     void printDBTable(const std::string& tableName) {
         m_dbManager->printAllRecords(tableName);

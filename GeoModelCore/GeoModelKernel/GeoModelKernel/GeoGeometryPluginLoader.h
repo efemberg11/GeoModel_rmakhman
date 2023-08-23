4fb289707cbacd0634fab6927c73244015a7d77d
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2023-08-23 11:24:39 +0200

    Include GeoVGeometryPlugin
    Apparently, the `GeoVGeometryPlugin` needs to be explicitly included in `GeoGeometryPluginLoader` now.
    
    This commit fixes the build of GeoModelATLAS 'master', and closes issue
    https://gitlab.cern.ch/atlas/geomodelatlas/GeoModelATLAS/-/issues/10

--- GeoModelCore/GeoModelKernel/GeoModelKernel/GeoGeometryPluginLoader.h
+++ GeoModelCore/GeoModelKernel/GeoModelKernel/GeoGeometryPluginLoader.h
@@ -5,5 +5,6 @@
 #ifndef GEOGEOMETRYPLUGINLOADER_H_
 #define GEOGEOMETRYPLUGINLOADER_H_
 #include "GeoModelKernel/GeoPluginLoader.h"
+#include "GeoModelKernel/GeoVGeometryPlugin.h"
 typedef GeoPluginLoader<GeoVGeometryPlugin> GeoGeometryPluginLoader;
 #endif

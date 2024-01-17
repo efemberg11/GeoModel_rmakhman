fd763025fccd63f6bbafd12d67d02ec0dcd4104f
Johannes Josef Junggeburth <johannes.josef.junggeburth@cern.ch>
2024-01-17 15:42:44 +0100

    Introduce shape & volume sorters
    Add check for GeoTransforms
    
    Factor out the log vol sorter
    
    Exclude full physical volumes from the check

---
+++ GeoModelTools/GeoModelFuncSnippets/GeoModelFuncSnippets/GeoShapeSorter.h
@@ -0,0 +1,45 @@
+/*
+  Copyright (C) 2002-2024 CERN for the benefit of the ATLAS collaboration
+*/
+#ifndef GEOMODELFUNCSNIPPETS_GeoShapeSorter_H
+#define GEOMODELFUNCSNIPPETS_GeoShapeSorter_H
+
+#include "GeoModelKernel/GeoShape.h"
+#include "GeoModelKernel/GeoIntrusivePtr.h"
+#include <set>
+
+/// @brief  Helper struct to construct set of GeoShapes where the defining parameters of
+///         each element are unique within 1 micrometer
+struct GeoShapeSorter {
+   template<class ShapeType1, class ShapeType2>
+    bool operator()(const GeoIntrusivePtr<ShapeType1>& a, 
+                    const GeoIntrusivePtr<ShapeType2>& b) const {
+            return (*this)(a.get(), b.get());
+    }
+
+    bool operator()(const GeoShape* a, const GeoShape* b) const;
+    /// @brief Compares 2 GeoShapes
+    /// @param a : Pointer to shape A
+    /// @param b : Pointer to shape B
+    /// @return  Returns 0 if no defining shape property could be found that differs
+    ///          Returns -1 if the first defining & differing property of A is smaller
+    ///          Returns 1 otherwise
+    int compare(const GeoShape* a, const GeoShape* b) const;
+
+};
+
+struct GeoComposedShapeSorter {
+    template<class ShapeType1, class ShapeType2>
+    bool operator()(const GeoIntrusivePtr<ShapeType1>& a, 
+                    const GeoIntrusivePtr<ShapeType2>& b) const {
+            return (*this)(a.get(), b.get());
+    }
+    bool operator()(const GeoShape* a, const GeoShape* b) const;
+
+};
+/// @brief 
+/// @tparam ShapeType 
+template<class ShapeType>
+using GeoShapeSet = std::set<GeoIntrusivePtr<const ShapeType>, GeoShapeSorter>;
+
+#endif
---
+++ GeoModelTools/GeoModelFuncSnippets/src/GeoLogVolSorter.cxx
@@ -0,0 +1,20 @@
+/*
+  Copyright (C) 2002-2024 CERN for the benefit of the ATLAS collaboration
+*/
+#include "GeoModelFuncSnippets/GeoLogVolSorter.h"
+#include "GeoModelFuncSnippets/throwExcept.h"
+#include "GeoModelFuncSnippets/GeoShapeSorter.h"
+
+bool GeoLogVolSorter::operator()(const GeoLogVol* a, const GeoLogVol* b) const{
+    return compare(a, b) < 0;
+}
+int GeoLogVolSorter::compare(const GeoLogVol*a, const GeoLogVol* b) const{
+    if (!a || !b) {
+        THROW_EXCEPTION("Nullptr given to the comparator");
+    }
+    if (a->getMaterial() != b->getMaterial()) {
+        return a->getMaterial()->getName() < b->getMaterial()->getName() ? -1 : 1;
+    }
+    static const GeoShapeSorter shapeSorter{};
+    return shapeSorter.compare(a->getShape(), b->getShape());
+}

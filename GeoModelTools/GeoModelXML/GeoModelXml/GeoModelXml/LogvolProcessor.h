c4d0062e6e68b53bfbf0c1edae213d41540c0970
Johannes Junggeburth <johannes.josef.junggeburth@cern.ch>
2024-02-16 00:04:24 +0100

    GeoModelXml - Implement sharing mechanism for LogVol & non FullPhysVols

--- GeoModelTools/GeoModelXML/GeoModelXml/GeoModelXml/LogvolProcessor.h
+++ GeoModelTools/GeoModelXML/GeoModelXml/GeoModelXml/LogvolProcessor.h
@@ -12,24 +12,21 @@
 #include <map>
 
 #include "GeoModelXml/ElementProcessor.h"
+#include "GeoModelKernel/GeoNameTag.h"
 class GmxUtil;
-class GeoNameTag;
-class GeoLogVol;
-class GeoPhysVol;
 
 class LogvolProcessor: public ElementProcessor {
 public:
-    typedef struct {
-        GeoNameTag *name;
-        int id;
-        GeoLogVol *logVol;
-        bool alignable;
-    } LogVolStore; 
+     struct LogVolStore {
+        GeoIntrusivePtr<GeoNameTag> name{};
+        int id{0};
+        GeoLogVolPtr logVol{};
+        bool alignable{false};
+    } ; 
     void process(const xercesc::DOMElement *element, GmxUtil &gmxUtil, GeoNodeList &toAdd);
     void zeroId(const xercesc::DOMElement *element);
 private:
     std::map<std::string, LogVolStore> m_map; 
-    std::map<std::string, GeoPhysVol*> m_mapPV; 
 };
 
 #endif // LOGVOL_PROCESSOR_H

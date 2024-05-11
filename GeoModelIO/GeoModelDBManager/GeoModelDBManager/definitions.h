056650ed6e603699f72354fe4fca4b8ac2e4834f
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2024-05-11 03:02:44 +0200

    Add definitions for GeoModel I/O types

---
+++ GeoModelIO/GeoModelDBManager/GeoModelDBManager/definitions.h
@@ -0,0 +1,24 @@
+// Copyright (C) 2002-2024 CERN for the benefit of the ATLAS collaboration
+
+/*
+ * This header file provides global definitions used 
+ * in the I/O of GeoModel into and from an SQLite file. 
+ *
+ *  Author:     Riccardo Maria BIANCHI @ CERN
+ *  Created on: May, 2024
+ *
+ */
+
+
+#ifndef GEOMODELDBMANAGER_DEFINITIONS_H
+#define GEOMODELDBMANAGER_DEFINITIONS_H
+
+#include <vector>
+#include <variant>
+
+// define the data types used to retrieve and store data from the DB
+using DBRecordEntry = std::variant<int,long,float,double,std::string>;
+using DBRowEntry = std::vector<DBRecordEntry>;
+using DBRowsList = std::vector<DBRowEntry>;
+
+#endif

f3a5364cc0a3fe5432f11c40960bc3920d8c2faf
Vakho Tsulaia <tsulaia@cern.ch>
2025-04-16 08:56:46 -0700

    Dropped the unused GeoModelG4/GeoModel2G4/original_GeoModel2G4

--- GeoModelG4/GeoModel2G4/original_GeoModel2G4/src/components/Geo2G4_entries.cxx
+++
@@ -1,7 +0,0 @@
-#include "../Geo2G4Svc.h"
-#include "../GeoDetectorTool.h"
-#include "../GDMLDetectorTool.h"
-
-DECLARE_COMPONENT( Geo2G4Svc )
-DECLARE_COMPONENT( GeoDetectorTool )
-DECLARE_COMPONENT( GDMLDetectorTool )
